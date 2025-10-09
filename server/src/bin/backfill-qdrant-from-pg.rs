9fa9dafc18632ec67306409b7bf06360ad8d222b
cdxker <denzell@mintlify.com>
2025-10-09 11:27:55 -0700

    feature: remove dataset-id filter for backfill-qdrant-from-pg script

--- server/src/bin/backfill-qdrant-from-pg.rs
+++ server/src/bin/backfill-qdrant-from-pg.rs
@@ -72,10 +72,6 @@ async fn main() -> Result<(), ServiceError> {
 
     let mut offset = Some(start_offset);
 
-    let dataset_id = std::env::var("dataset_id")
-        .map(|dataset_str| uuid::Uuid::parse_str(&dataset_str).ok().unwrap())
-        .unwrap_or(uuid::Uuid::max());
-
     while let Some(cur_offset) = offset {
         use trieve_server::data::schema::chunk_group_bookmarks::dsl as chunk_group_bookmarks_columns;
         use trieve_server::data::schema::chunk_metadata::dsl as chunk_metadata_columns;
@@ -85,16 +81,12 @@ async fn main() -> Result<(), ServiceError> {
             ServiceError::BadRequest("Could not get database connection".to_string())
         })?;
 
-        println!("Fetching from postgres @ {}", cur_offset);
-
-        // Get postgres_fetch_points_count chunk_metadata
         let qdrant_dataset_id_pairs = chunk_metadata_columns::chunk_metadata
             .select((
                 chunk_metadata_columns::id,
                 chunk_metadata_columns::qdrant_point_id,
                 chunk_metadata_columns::dataset_id,
             ))
-            .filter(chunk_metadata_columns::dataset_id.eq(dataset_id))
             .filter(chunk_metadata_columns::id.ge(cur_offset))
             .order_by(chunk_metadata_columns::id)
             .limit(postgres_fetch_points_count)
